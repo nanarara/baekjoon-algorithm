9bcca8d06722797201aaa5ae3596348bf21c0404
NARA <nara.yoon@gmail.com>
2017-09-24 16:15:11 +0900

    delete statement

--- no2309/Main.java
+++ no2309/Main.java
@@ -22,11 +22,9 @@ public class Main {
 			for (int i = 0; i < 9; i++) {
 				for (int j = i + 1; j < 9; j++) {
 					int sum = 0;
-					if (i != j) {
-						for (int k = 0; k < 9; k++) {
-							if (k != i && k != j) {
-								sum += arr[k];
-							}
+					for (int k = 0; k < 9; k++) {
+						if (k != i && k != j) {
+							sum += arr[k];
 						}
 					}
 					if (sum == 100) {
@@ -39,8 +37,6 @@ public class Main {
 					}
 				}
 			}
-			
-			br.close();
 
 		} catch (Exception e) {
 			// TODO: handle exception
